08fb85f9fe7d44a4871e3906b1ce324c94c38b90
Robin Jacquemart <jacquemart.rob@gmail.com>
2020-08-18 11:25:01 +0200

    Exo 6

--- docker-lamp/htdocs/index.php
+++ docker-lamp/htdocs/index.php
@@ -46,23 +46,94 @@ if($now > '05:00' AND $now < '09:00'){
 }
 
 // 3. "Different greetings according to age" Exercise
+//4 "Ajouter le genres
+//5 Ajouter langue
+$radioVal = $_GET['gender'];
+$language = $_GET['language'];
+// Variable pour changer le texte
+$english = NULL;
 
-if (isset($_GET["age"])){
-	if($_GET["age"] < 12) {
-		echo "Hello kiddo !";
-	} else if( 12 < $_GET["age"] AND $_GET["age"] < 18 ){
-		echo "Hello Teenager !";
-	} else if (18 < $_GET["age"] AND $_GET["age"] < 115) {
-		echo "Hello adult !";
-	} else if( 115 < $_GET["age"]){
-		echo "Wow! Still alive ? Are you a robot, like me ? Can I hug you ?";
+if (isset($_GET["age"]) AND isset($radioVal) AND isset($language)){
+	// Change le texte en fonction de la réponse
+	if($language == 'yes'){
+		$english = "Hello";
+	} else {
+		$english = "Aloha";
 	}
-}
+	//Female
+	if($radioVal == 'female'){
+		if($_GET["age"] <= 12) {
+			echo "Hello Miss Teen !";
+		} else if( 12 < $_GET["age"] AND $_GET["age"] <= 18 ){
+			echo "$english  Miss Teenager !";
+		} else if (18 < $_GET["age"] AND $_GET["age"] < 115) {
+			echo "$english  Madam !";
+		} else if( 115 <= $_GET["age"]){
+			echo "$english Madam! Still alive ? Are you a robot, like me ? Can I hug you ?";
+		}
+	} else if ($radioVal == 'male'){ //male
+		if($_GET["age"] <= 12) {
+			echo "$englishMister Teen !";
+		} else if( 12 < $_GET["age"] AND $_GET["age"] <= 18 ){
+			echo "$english Mister Teenager !";
+		} else if (18 < $_GET["age"] AND $_GET["age"] < 115) {
+			echo "$english  Sir !";
+		} else if( 115 <= $_GET["age"]){
+			echo "$english Sir! Still alive ? Are you a robot, like me ? Can I hug you ?";
+		}
+	}
+} 
 
 //Form incomplète
 ?>
 <form method="get" action="">
 	<label for="age">Please type your age :</label>
 	<input type="text" name="age">
+
+	<p>Male or female  ?</p>
+	<input type="radio" name="gender" value="male">
+	<label for="male">Male</label><br />
+
+	<input type="radio" name="gender" value ="female">
+	<label for='female'>Female</label><br />
+
+	<label for="language">Do you speak English ?</label><br />
+	<input type="radio" name='language' value="yes">
+	<label for='yes'>Yes</label><br />
+	<input type="radio" name='language' value="no">
+	<label for='no'>No</label><br />
+
+	<input type="submit" name="submit" value="Greet me now">
+</form>
+
+<?php
+// The Girl Soccer Team
+$name = $_GET["sName"];
+$gender = $_GET["sGender"];
+$age = $_GET["sAge"];
+
+if(isset($name) AND isset($gender) AND isset($age)){
+	if($gender == "sGirl" AND 21 < $age AND $age < 40){
+		echo "Welcome to the team $name !";
+	} else {
+		echo "Sorry $name you don't fit the criteria..";
+	}
+} 
+?>
+
+<form method="get" action ="">
+	<p> Insert your information for the soccer Team selection :</p>
+	<label for="sName">Enter your name :</label>
+	<input type="text" name="sName"><br />
+
+	<input type="radio" name="sGender" value ="sGirl">
+	<label for="sGirl">Girl</label><br />
+
+	<input type="radio" name="sGender" value="sBoy">
+	<label for="sBoy">Boy</label><br />
+
+	<label for="sAge">Enter your age :</label>
+	<input type="text" name="sAge"><br />
+
 	<input type="submit" name="submit" value="Greet me now">
 </form>
