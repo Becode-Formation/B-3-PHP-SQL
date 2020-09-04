e18a2cff6e3530bf9d706cd8fd2b98e926d03bca
Robin Jacquemart <jacquemart.rob@gmail.com>
2020-09-04 09:42:14 +0200

    Etape 1 POO

--- POO/EXO_1/index.php
+++ POO/EXO_1/index.php
@@ -1,7 +1,7 @@
 <?php
 
 class Form{
-
+    // Création du formulaire ou fermeture 
     public function create($pos){
         if($pos == "start"){
             return "<form method='post'>";
@@ -9,21 +9,29 @@ class Form{
             return "</form>";            
         }
     }
+    // Création des inputs "simple"
     public function input($type, $inputName){
         return "<input type=' ".$type." ' name=' ".$inputName." '/>";
     }
-
+    //Création d'input select
     public function select($selectName, ...$optionValue){
         forEach ($optionValue as $n){
             $opt .= "<option value='".$n."'>".$n."</option>";
         }
         return "<select name=' ".$selectName." '>" . $opt . "</select>";
     }
-
+    //Création de textearea  ( sans blague ?)
     public function textArea($areaName){
         return "<textarea name=' " .$areaName. " ' ></textarea>";
     }
-
+    // Création de radio button
+    public function radio($radioName, ...$radioValue){
+        forEach($radioValue as $value){
+            $radio .=  "<label for = ' " . $radioName . " ' >" . $value. "</label> <input type='radio' id= ' " .$value . " ' name =' " . $radioName . " ' value=' " . $value . " ' />"; 
+        }
+        return "<label for=' " . $radioName . " '>" . $radioName . "</label> " . $radio;
+    }
+    // Création du bouton
     public function submit(){
         return "<button type='submit'>Envoyer</button>";
     }
@@ -35,6 +43,7 @@ echo $form->input("text", "name");
 echo $form->input("email", "email");
 echo $form->select("car", "Volvo", "Citroen", "Mercedes");
 echo $form->textArea("msg");
+echo $form->radio("gender", "Homme", "Femme", "Other");
 echo $form->submit();
 echo $form->create("end");
 ?>
